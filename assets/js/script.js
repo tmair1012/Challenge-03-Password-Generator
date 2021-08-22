9179200996b8fd2792ad4a56999e2fead89b5d39
Tyler <tyleramair@gmail.com>
2021-08-21 19:46:58 -0700

    got prompts to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,81 @@
 // Assignment code here
-var specialCharacters = " !$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var alphabetLower = "abcdefghijklmnopqrstuvwxyz";
-var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var Numbers = "0123456789";
-
-var upperLower = [alphabetLower, alphabetUpper];
-var upperLowerCharacters = [alphabetLower, alphabetUpper, specialCharacters];
-var upperLowerNumbers = [AlphabetLower, alphabetUpper, Numbers];
-var allCharacters = [specialCharacters, alphabetLower, alphabetUpper, Numbers];
-
-function generatePassword(){
-  allCharacters[Math.floor(Math.random()*allCharacters.length)];
-  alert (allCharacters);}
-  generatePassword();
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var specialCharacters = "!$%&'()*+,-./:;<=>?@[^_`{|}~";
+var alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];;
+var alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];;
+var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const passwordAdditions =[alphabetLower];
+var passwordLength = alphabetUpper.length;
+var yourPassword =" ";
+var passwordCharacters = min = 8, max = 128;
 
 function generatePassword(){
-
-}
-// Write password to the #password input
-function writePassword() {
-  var createpassword = window.prompt ("Would you Like to generate a new password?");
+for (let i = 0; i < passwordLength.length; i++) {
   
+  yourPassword += passwordAdditions.charAt(Math.floor(Math.random()
+  * passwordLength))
+  
+}
+}
+
+
+  console.log (alphabetLower);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var generateBtn = document.querySelector("#generate");
   
- if (createpassword === "yes" || createpassword === "YES" || createpassword === "Y" || createpassword === "Yes" ){
-   var passwordCharacters = window.prompt ("How many characters would you like in your password?");
- 
+  
+  
+  //write the password function
+  function writePassword() {
+    var createpassword = window.prompt ("Would you Like to generate a new password?");
+    
+    //this tells that the person wants to generate a password
+   if (createpassword === "yes" || createpassword === "YES" || createpassword === "Y" || createpassword === "Yes" ){
+     // add the choose characters function here
+    passwordCharacters = window.prompt ("How many characters would you like in your password? (8-128)");
+    if (passwordCharacters <=128 && passwordCharacters >=8){
+
+    var getUpper = window.confirm ("Would you like to add Uppercase letters to your password?");
+    if (getUpper){
+      passwordAdditions.push(alphabetUpper);
+    }
+    
+    var getNumbers = window.confirm ("Would you like to add Numbers to your password?");
+    if (getNumbers){
+      passwordAdditions.push(Numbers);
+    }
+    var getSpecialCharacters = window.confirm ("Would you like to add special characters to your password?");
+    if (getSpecialCharacters) {
+      passwordAdditions.push(specialCharacters)
+    }
+    console.log(passwordAdditions);
 }
-   if  (createpassword === "no") {
-    var noPassword = alert ("Okay have fun doing it yourself!");
-   }
-else {
-     var youDidItWrong = window.prompt ("Please enter yes or no, its quite simple.");
+  else{
+    alert ('please enter a number between 8 and 128')
+  }
+  }
+    // need to restart or exit function here
+     if  (createpassword === "no") {
+      var noPassword = alert ("Okay have fun doing it yourself!");
+
+     }
+  else {
+       var youDidItWrong = alert ("Please enter yes or no, its quite simple.");
   
-   }
- 
+
+      }
+  }
+  
+// Get references to the #generate element
+
+
+
+// Write password to the #password input
+
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
