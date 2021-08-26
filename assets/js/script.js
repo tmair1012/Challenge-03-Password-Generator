9dad8a31f30f73899fb35816681f0654a2279e11
TylerMair <tyleramair@gmail.com>
2021-08-25 23:33:19 -0700

    final, still wont display all characters, but console log does

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   // Write password to the #password input
-  passwordText.value = password;
+    passwordText.value = password;
+    
+  
 
 }
 
@@ -20,14 +22,17 @@ function generatePassword() {
   var alphabetLower = "abcdefghijklmnopqrstuvwxyz";
   var Numbers = "0123456789";
   var passwordAdditions = "";
-  var yourPassword = " ";
+
+  
+  
   var createpassword = window.prompt("Would you Like to generate a new password?");
 
   //this tells that the person wants to generate a password
   if (createpassword === "yes" || createpassword === "YES" || createpassword === "Y" || createpassword === "Yes") {
     // add the choose characters function here
-    passwordCharacters = window.prompt("How many characters would you like in your password? (8-128)");
+   var passwordCharacters = window.prompt("How many characters would you like in your password? (8-128)");
     if (passwordCharacters <= 128 && passwordCharacters >= 8) {
+      passwordAdditions += alphabetLower;
       var getUpper = window.confirm("Would you like to add Uppercase letters to your password? Ok for 'yes' cancel for 'no'");
       if (getUpper) {
         passwordAdditions += alphabetUpper;
@@ -41,15 +46,20 @@ function generatePassword() {
       if (getSpecialCharacters) {
         passwordAdditions += specialCharacters;
       }
-      generatePassword();
-      var password = generatePassword();
 
+      for (let i = 0; i < passwordCharacters; i++) {
+        var yourPassword = passwordAdditions.charAt(Math.floor(Math.random() * passwordAdditions.length));
+        
+        console.log(yourPassword);
+        
+      }
+      return yourPassword;
     }
     else {
       alert('please enter a number between 8 and 128')
     }
   }
-  // need to restart or exit function here
+
   if (createpassword === "no") {
     alert("Okay have fun doing it yourself!");
 
@@ -57,34 +67,9 @@ function generatePassword() {
   else {
     alert("Please enter yes or no, its quite simple.");
 
-
   }
-  for (let i = 0; i < passwordCharacters; i++) {
-    console.log(passwordAdditions.charAt(Math.random() * passwordAdditions.length));
-
-  }
-
+  
 }
-
-
-
-
-var passwordText = document.querySelector("#password");
-
-
-
-//write the password function
-function writePassword() {
-
-}
-
-
-
-
-
-
-
-
 
 
 // Add event listener to generate button
